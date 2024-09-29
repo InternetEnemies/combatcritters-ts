a9e760f570b3e3d825e6d5d713bea3740146013e
VanJackal <v@njackal.com>
2024-09-29 10:18:00 -0500

    basic rest implementation

--- src/rest/Rest.ts
+++ src/rest/Rest.ts
@@ -1,3 +1,53 @@
-import * as API from "./payloads"
+import * as Payloads from "./payloads"
+import {IRest} from "./IRest";
+import axios, {AxiosInstance, AxiosRequestConfig} from "axios"
 
-export class Rest {}
+export class Rest  implements IRest {
+    private Axios:AxiosInstance;
+    
+    constructor(private readonly api: string) {
+        this.Axios = axios.create({
+            baseURL:api,
+            withCredentials: true
+        })
+    }
+    private async request(config:AxiosRequestConfig) {
+        // todo at some point this function should also handle/wrap errors
+        let res = (await this.Axios.request(config)).data
+    }
+
+    async get(resource: string) {
+        return this.request({
+            method: "get",
+            url: resource
+        })
+    }
+    async delete(resource: string) {
+        return this.request({
+            method: "delete",
+            url: resource
+        })
+    }
+    async put(resource: string, payload: any) {
+        return this.request({
+            method: "put",
+            url: resource,
+            data: payload
+        })
+    }
+    async post(resource: string, payload: any) {
+        return this.request({
+            method: "post",
+            url: resource,
+            data: payload
+        })
+    }
+    async patch(resource: string, payload: any) {
+        return this.request({
+            method: "patch",
+            url: resource,
+            data: payload
+        })
+    }
+    
+}
---
+++ src/rest/index.ts
@@ -0,0 +1,3 @@
+export * from "./Rest"
+export * from "./IRest"
+export * as Payloads from "./payloads"
