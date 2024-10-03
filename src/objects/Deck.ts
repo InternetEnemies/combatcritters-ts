d117576cd23c7869f1be60ed9b1296e98ecd6f9a
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-03 08:42:41 -0500

    deck object: addCard

--- src/objects/Deck.ts
+++ src/objects/Deck.ts
@@ -1,19 +1,28 @@
+import { Client } from "../Client";
+import { Rest } from "../rest";
+import { Routes } from "../rest/routes/decks";
 import { ICard } from "./interfaces";
-import {DeckValidity, IDeck} from "./interfaces/IDeck";
+import { DeckValidity, IDeck } from "./interfaces/IDeck";
 
 export class Deck implements IDeck {
-    private readonly _deckid: number;
-    private readonly _name: string;
-    private readonly _cards: ICard[];
+    private _deckid: number;
+    private _name: string;
+    private _cards: ICard[];
+    private readonly _client: Client;
 
-    constructor(deckid: number, name: string, cards: ICard[]) {
-        this._deckid = deckid;
-        this._name = name;
+    constructor(deckid: number, name: string, cards: ICard[], client: Client) {
+        this._deckid = Object.freeze(deckid);
+        this._name = Object.freeze(name);
         this._cards = cards;
+        this._client = client;
     }
 
     public async addCard(card: ICard, position: number): Promise<DeckValidity> {
-        throw new Error("Method not implemented.");
+        var temp_cards = this._cards;
+        temp_cards.splice(position, 0, card);
+        const response = await this._client.rest.put(Routes.User.deckCards(this._client.user.userid ,this._deckid), { cards: temp_cards });
+        this._cards = Object.freeze(response.data.deck);
+        return response.data.validity;
     }
     
     public async removeCard(position: number): Promise<DeckValidity> {
