40f58e65863150ed3392baf818d96a21d5de0d77
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-03 18:02:23 -0500

    UserCardsManager: getCards

--- src/managers/UserCardsManager.ts
+++ src/managers/UserCardsManager.ts
@@ -1,16 +1,25 @@
 import { ICard } from "../objects";
 import { ICardQuery } from "../objects/interfaces/ICardQuery";
 import { ICardQueryBuilder } from "../objects/interfaces/ICardQueryBuilder";
-import {IUserCardsManager} from "./interfaces";
+import { IUserCardsManager } from "./interfaces";
+import { IClient } from "../IClient";
 
 export class UserCardsManager implements IUserCardsManager {
-    getCard(id: number): Promise<ICard> {
+    private readonly _client: IClient;
+
+    constructor(client:IClient){
+        this._client = client;
+    }
+
+    public async getCard(id: number): Promise<ICard> {
         throw new Error("Method not implemented.");
     }
-    getCards(query: ICardQuery): Promise<ICard[]> {
+
+    public async getCards(query: ICardQuery): Promise<ICard[]> {
         throw new Error("Method not implemented.");
     }
-    getBuilder(): ICardQueryBuilder {
+
+    public getBuilder(): ICardQueryBuilder {
         throw new Error("Method not implemented.");
     }
 }
