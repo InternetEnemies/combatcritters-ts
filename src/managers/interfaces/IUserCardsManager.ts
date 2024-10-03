059ce13fcbcf1efc7423c9b49d314c8e1dfce841
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-03 13:15:42 -0500

    added builder getter

--- src/managers/interfaces/IUserCardsManager.ts
+++ src/managers/interfaces/IUserCardsManager.ts
@@ -1,7 +1,9 @@
 import { ICard } from "../../objects";
 import { ICardQuery } from "../../objects/interfaces/ICardQuery";
+import { ICardQueryBuilder } from "../../objects/interfaces/ICardQueryBuilder";
 
 export interface IUserCardsManager {
     getCard(id: number): Promise<ICard>; // This method set the CardQuery.owned to be yes
     getCards(query: ICardQuery): Promise<ICard[]>; // This method set the CardQuery.owned to be yes
+    getBuilder(): ICardQueryBuilder;
 }
