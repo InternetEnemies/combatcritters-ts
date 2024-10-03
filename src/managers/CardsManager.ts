cbe63cbf63c75306b0d7353448840304c622e755
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-03 17:28:06 -0500

    need to wait for card fromPayload

--- src/managers/CardsManager.ts
+++ src/managers/CardsManager.ts
@@ -1,5 +1,42 @@
-import {ICardsManager} from "./interfaces";
+import { IClient } from "../IClient";
+import { ICard } from "../objects";
+import { ICardQuery } from "../objects/interfaces/ICardQuery";
+import { ICardQueryBuilder } from "../objects/interfaces/ICardQueryBuilder";
+import { Routes } from "../rest";
+import  {ICardsManager } from "./interfaces";
+import { Card as CardPayload } from "../rest/payloads";
 
 export class CardsManager implements ICardsManager {
-    //todo
+    private readonly _client: IClient;
+
+    constructor(client: IClient) {
+        this._client = client;
+    }
+
+    public async getCard(id: number): Promise<ICard> {
+        const userRes:CardPayload = await this._client.rest.get(Routes.Cards.singleCard(id));
+        return new ICard(userRes);
+    }
+
+    public async getCards(query: ICardQuery): Promise<ICard[]> {
+        const queryParams = [
+            "costGreater=" + query.costGreater,
+            "costLess=" + query.costLess,
+            "id=" + query.ids.join(","),
+            "order=" + query.order,
+            "owned=" + query.owned,
+            "rarityExclude=" + query.rarityExclude,
+            "rarityInclude=" + query.rarityInclude
+        ].join("&");
+        const userRes:CardPayload[] = await this._client.rest.get(Routes.Cards.cards(queryParams));
+        const cards: ICard[] = [];
+        for (let i = 0; i < userRes.length; i++) {
+            cards.push(new ICard(userRes[i]));
+        }
+        return cards;
+    }
+
+    public getBuilder(): ICardQueryBuilder {
+        throw new Error("Method not implemented.");
+    }
 }
--- src/managers/UserCardsManager.ts
+++ src/managers/UserCardsManager.ts
@@ -1,5 +1,16 @@
+import { ICard } from "../objects";
+import { ICardQuery } from "../objects/interfaces/ICardQuery";
+import { ICardQueryBuilder } from "../objects/interfaces/ICardQueryBuilder";
 import {IUserCardsManager} from "./interfaces";
 
 export class UserCardsManager implements IUserCardsManager {
-    //todo
+    getCard(id: number): Promise<ICard> {
+        throw new Error("Method not implemented.");
+    }
+    getCards(query: ICardQuery): Promise<ICard[]> {
+        throw new Error("Method not implemented.");
+    }
+    getBuilder(): ICardQueryBuilder {
+        throw new Error("Method not implemented.");
+    }
 }
