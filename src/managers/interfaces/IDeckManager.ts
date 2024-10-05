5f9f1301f060af783a5e4bfce67f048df815ec6b
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-04 21:00:30 -0500

    fixed: deckManager interface

--- src/managers/interfaces/IDeckManager.ts
+++ src/managers/interfaces/IDeckManager.ts
@@ -3,7 +3,7 @@ import { DeckRules } from "../../rest/payloads";
 
 export interface IDeckManager {
     getDecks(): Promise<IDeck[]>;
-    addDeck(deckName: string): Promise<IDeck>;
-    deleteDeck(deckID: number): Promise<void>;
+    createDeck(deckName: string): Promise<IDeck>;
+    deleteDeck(deck: IDeck): Promise<void>;
     getDeckRules(): Promise<DeckRules>;
 }
