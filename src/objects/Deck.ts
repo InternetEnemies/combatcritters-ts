036e3a12d924e973a7f3648eeae89da511b0080c
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-07 01:42:31 -0500

    Deck: getCards

--- src/objects/Deck.ts
+++ src/objects/Deck.ts
@@ -1,30 +1,39 @@
 import { Client } from "../Client";
-import { Rest } from "../rest";
 import { Routes } from "../rest/routes/decks";
-import { ICard } from "./interfaces";
+import { ICard, IUser } from "./interfaces";
 import { DeckValidity, IDeck } from "./interfaces/IDeck";
-import { Deck as DeckPayload, DeckDetails } from "../rest/payloads/decks";
+import { Deck as DeckPayload, DeckDetails as DeckDetailsPayload } from "../rest/payloads/decks";
+import { Card as CardPayload } from "../rest/payloads/cards";
+import { Card } from "./Card";
 
 export class Deck implements IDeck {
-    private _deckid: number;
+    private readonly _deckid: number;
     private _name: string;
-    private _cards: ICard[];
+    private readonly _user: IUser;
+    private _cards!: number[];
     private readonly _client: Client;
 
-    public static fromDeckPayload(detailsPayload:DeckDetails , deckPayload: DeckPayload, client: Client): Deck {
-        return new Deck(
-            detailsPayload.deckid,
-            detailsPayload.name,
-            deckPayload.cards, 
-            client
-        );
+    public static fromDeckDetailsPayload(payload: DeckDetailsPayload, client: Client, user:IUser): Deck {
+        return new Deck(payload.deckid, 
+                        payload.name, 
+                        client,
+                        user);
     }
 
-    constructor(deckid: number, name: string, cards: ICard[], client: Client) {
-        this._deckid = Object.freeze(deckid);
+    constructor(deckid: number, name: string, client: Client, user:IUser) {
+        this._deckid = deckid;
         this._name = Object.freeze(name);
-        this._cards = cards;
         this._client = client;
+        this._user = user;
+    }
+
+    public async getCards(): Promise<ICard[]> {
+        const response:CardPayload[] = await this._client.rest.get(Routes.User.deckCards(this._user.userid ,this._deckid));
+        let cardsObj: ICard[] = [];
+        response.forEach((card) => {
+            cardsObj.push(Card.fromCardPayload(card));
+        });
+        return cardsObj;
     }
 
     public async addCard(card: ICard, position: number): Promise<DeckValidity> {
@@ -43,8 +52,10 @@ export class Deck implements IDeck {
         return response.data.validity;
     }
 
-    public async delete(): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async setCards(cards: ICard[]): Promise<DeckValidity> {
+        const response = await this._client.rest.put(Routes.User.deckCards(this._client.user.userid ,this._deckid), { cards });
+        this._cards = Object.freeze(response.data.deck);
+        return response.data.validity;
     }
 
     public async getValidity(): Promise<DeckValidity> {
@@ -57,9 +68,5 @@ export class Deck implements IDeck {
 
     public get name(): string {
         return this._name;
-    }
-
-    public get cards(): ICard[] {
-        return this._cards;
     }
 }
