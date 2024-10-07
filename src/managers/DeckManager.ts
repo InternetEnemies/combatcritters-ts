3cd3f0257d8239c0cb91f8374ba3f8ea868794c2
Hugo Ng <ym7ym7228@gmail.com>
2024-10-07 15:13:38 -0500

    deckmanager: skeleton code

--- src/managers/DeckManager.ts
+++ src/managers/DeckManager.ts
@@ -1,5 +1,27 @@
+import { IClient } from "../IClient";
+import { IDeck, IUser } from "../objects";
+import { DeckRules } from "../rest/payloads";
 import {IDeckManager} from "./interfaces";
 
 export class DeckManager implements IDeckManager {
-    //todo
+    private readonly _client: IClient;
+    private readonly _user: IUser;
+
+    constructor(client: IClient, user: IUser) {
+        this._client = client;
+        this._user = user;
+    }
+
+    getDecks(): Promise<IDeck[]> {
+        throw new Error("Method not implemented.");
+    }
+    createDeck(deckName: string): Promise<IDeck> {
+        throw new Error("Method not implemented.");
+    }
+    deleteDeck(deck: IDeck): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    getDeckRules(): Promise<DeckRules> {
+        throw new Error("Method not implemented.");
+    }
 }
