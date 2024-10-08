67e131b43f8e54d833c78faa30f01f5bc22190f8
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-08 14:44:46 -0500

    update: IProfile functions

--- src/objects/interfaces/IProfile.ts
+++ src/objects/interfaces/IProfile.ts
@@ -1,3 +1,14 @@
-export interface IProfile {
+import { IDeck } from "./index";
 
+export interface IProfile {
+    /**
+     * Get the profile of the user, which is the feature deck
+     * @returns Promise<IDeck> the feature deck of the user
+     */
+    getProfile(): Promise<IDeck>;
+    /**
+     * Set the profile of the user, which is the feature deck
+     * @param deck the deck to set as the feature deck
+     */
+    setProfile(deck:IDeck): Promise<void>;
 }
