8dca7c50d3dac7e8b60d3a87b7ea475b2e038a15
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-08 13:33:13 -0500

    FriendsManager: functions

--- src/managers/FriendsManager.ts
+++ src/managers/FriendsManager.ts
@@ -1,5 +1,24 @@
-import { IFriendsManager } from "./interfaces";
+import { IClient } from "../index";
+import { IUser } from "../objects/index";
+import { IFriendsManager } from "./index";
 
-export class Friends implements IFriendsManager {
+export class FriendsManager implements IFriendsManager {
+    private readonly _user: IUser;
+    private readonly _client: IClient;
+
+    constructor(client: IClient, user: IUser) {
+        this._client = client;
+        this._user = user;
+    }
+
+    getFriends(): Promise<IUser[]> {
+        throw new Error("Method not implemented.");
+    }
+    addFriend(user: IUser): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    getFriendsRequests(): Promise<IUser[]> {
+        throw new Error("Method not implemented.");
+    }
     
 }
