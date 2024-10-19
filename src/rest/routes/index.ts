1de64ca0b76ddc30a834f265080ebb8fab1a6d0c
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-19 15:50:41 -0500

    Added Packs routes

--- src/rest/routes/index.ts
+++ src/rest/routes/index.ts
@@ -3,11 +3,13 @@ import {Routes as Cards} from './cards'
 import {Routes as Decks} from './decks'
 import {Routes as Friends} from './friends'
 import {Routes as Profiles} from './profiles'
+import {Routes as Packs} from './packs'
 
 export const Routes = {
     Auth,
     Cards,
     Decks,
     Friends,
-    Profiles
+    Profiles,
+    Packs
 }
