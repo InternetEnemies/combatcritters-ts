13598b29554c43892348667c1d72e70a69470112
jackal <v@njackal.com>
2024-10-22 13:50:13 -0500

    fixed test hanging

--- test/integration/auth.spec.ts
+++ test/integration/auth.spec.ts
@@ -13,11 +13,10 @@ beforeEach(async () => {
 describe("Auth test", () => {
     
     // ! this test will fail the second time
-    it("should register, login, and access privileged route", async (done) => {
+    it("should register, login, and access privileged route", async () => {
         await client.register("username","password")
         await client.login("username","password")
         await client.rest.get("/ping");
-        done()
     })
 })
 
