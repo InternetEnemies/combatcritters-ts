7d10e004029fa37e3aaddf713cc9ebc54c84e318
Kevin Breckman <kevinbreckman@gmail.com>
2024-10-24 11:40:18 -0500

    Create stub

--- src/objects/Offer.ts
+++ src/objects/Offer.ts
@@ -1,36 +1,67 @@
-import { ICard, ICurrency, IItemStack, IOffer, IPack, ITradeItem } from "../index";
+import {
+  ICard,
+  ICurrency,
+  IItemStack,
+  IOffer,
+  IPack,
+  ItemStack,
+  ITradeItem,
+} from "../index";
 import { Offer as OfferPayload } from "../rest/payloads";
 
-export class Offer implements IOffer{
-    private readonly _offerID: number;
-    private readonly _receiveItem: IItemStack<ICurrency | ICard | IPack>;
-    private readonly _giveItem: IItemStack<ICurrency | ICard | IPack>[];
-    
-    public static fromOfferPayload(payload: OfferPayload): Offer {
-        //TODO: Implement this method
-        // https://github.com/InternetEnemies/combatcritters-ts/issues/61
-        throw new Error("Method not implemented.");
-    }
+export class Offer implements IOffer {
+  private readonly _offerID: number;
+  private readonly _receiveItem: IItemStack<ICurrency | ICard | IPack>;
+  private readonly _giveItem: IItemStack<ICurrency | ICard | IPack>[];
 
-    constructor(offerID: number, receiveItems: IItemStack<ICurrency | ICard | IPack>, giveItem: IItemStack<ICurrency | ICard | IPack>[]) {
-        this._offerID = offerID;
-        this._receiveItem = receiveItems;
-        this._giveItem = giveItem;
-    }
+  public static fromOfferPayload(payload: OfferPayload): Offer {
+    //TODO: Implement this method
+    // https://github.com/InternetEnemies/combatcritters-ts/issues/61
+    throw new Error("Method not implemented.");
+  }
 
-    public async compareUserItems(): Promise<ITradeItem<ICard | IPack | ICurrency>[]> {
-        //TODO: Implement this method
-        // https://github.com/InternetEnemies/combatcritters-ts/issues/61
-        throw new Error("Method not implemented.");
-    }
+  constructor(
+    offerID: number,
+    receiveItems: IItemStack<ICurrency | ICard | IPack>,
+    giveItem: IItemStack<ICurrency | ICard | IPack>[]
+  ) {
+    this._offerID = offerID;
+    this._receiveItem = receiveItems;
+    this._giveItem = giveItem;
+  }
 
-    public get offerID(): number {
-        return this._offerID;
+  public async compareUserItems(): Promise<
+    ITradeItem<ICard | IPack | ICurrency>[]
+  > {
+    //TODO: Implement this method
+    // https://github.com/InternetEnemies/combatcritters-ts/issues/61
+    const tradeItems: ITradeItem<IPack | ICard | ICurrency>[] = [];
+    for (let i = 0; i < this.giveItem.length; i++) {
+      if (i % 2 === 0) {
+        tradeItems.push({
+          giveItem: this._giveItem[i],
+          userItem: this._giveItem[i],
+        });
+      } else {
+        tradeItems.push({
+          giveItem: this._giveItem[i],
+          userItem: new ItemStack<IPack | ICurrency | ICard>(
+            this._giveItem[i].getItem(),
+            this._giveItem[i].getAmount() - 1
+          ),
+        });
+      }
     }
-    public get receiveItem(): IItemStack<ICurrency | ICard | IPack> {
-        return this._receiveItem;
-    }
-    public get giveItem(): IItemStack<ICurrency | ICard | IPack>[] {
-        return this._giveItem;
-    }
+    return tradeItems;
+  }
+
+  public get offerID(): number {
+    return this._offerID;
+  }
+  public get receiveItem(): IItemStack<ICurrency | ICard | IPack> {
+    return this._receiveItem;
+  }
+  public get giveItem(): IItemStack<ICurrency | ICard | IPack>[] {
+    return this._giveItem;
+  }
 }
