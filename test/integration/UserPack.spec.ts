6aef6ff5c2a89bdee558d63c9e84a91ac7605385
VanJackal <v@njackal.com>
2024-10-29 15:45:50 -0500

    fixed UserPack.open

---
+++ test/integration/UserPack.spec.ts
@@ -0,0 +1,26 @@
+import {BackendInstance} from "./ContainerUtil";
+import {IClient, IUserPack} from "../../src";
+import * as assert from "node:assert";
+
+let client: IClient;
+let container: BackendInstance
+
+beforeEach(async () => {
+    container = new BackendInstance()
+    client = await container.init();
+    await client.register("username","password")
+    await client.login("username","password")
+})
+describe("User Packs", () => {
+
+    it("User can open a pack", async () => {
+        let packs = await client.user.packs.getPacks()
+        let pack:IUserPack = packs.pop().getItem()
+        let cards = await pack.open();
+        assert.ok(cards.length > 0)
+    })
+})
+
+afterEach(async () => {
+    await container.teardown();
+})
