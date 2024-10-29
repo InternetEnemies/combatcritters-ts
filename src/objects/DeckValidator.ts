e67d3cdd2ef79dcdac1aa80f42b627c0cff44c25
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-29 03:15:47 -0500

    finished DeckValidator

--- src/objects/DeckValidator.ts
+++ src/objects/DeckValidator.ts
@@ -1,13 +1,14 @@
-import { ICard, ICardsManager, IDeck, IDeckValidator, IDeckValidity, } from "../index";
+import { CardItem, CardRarity, DeckValidity, ICard, ICardsManager, IDeck, IDeckValidator, IDeckValidity, } from "../index";
 import { DeckRules, DeckIssue } from "../rest/payloads";
 
 export class DeckValidator implements IDeckValidator {
-    private readonly min_cards: number;
-    private readonly max_cards: number;
-    private readonly limit_legend: number;
-    private readonly limit_epic: number;
-    private readonly limit_rare: number;
-    private readonly limit_item: number;
+    private readonly _min_cards: number;
+    private readonly _max_cards: number;
+    private readonly _limit_legend: number;
+    private readonly _limit_epic: number;
+    private readonly _limit_rare: number;
+    private readonly _limit_item: number;
+    private issues: Array<string>;
 
     public static fromDeckRules(rules: DeckRules): DeckValidator {
         return new DeckValidator(rules.min_cards, 
@@ -20,16 +21,70 @@ export class DeckValidator implements IDeckValidator {
     }
 
     constructor(min_cards: number, max_cards: number, limit_legend: number, limit_epic: number, limit_rare: number, limit_item: number) {
-        this.min_cards = min_cards;
-        this.max_cards = max_cards;
-        this.limit_legend = limit_legend;
-        this.limit_epic = limit_epic;
-        this.limit_rare = limit_rare;
-        this.limit_item = limit_item;
+        this._min_cards = min_cards;
+        this._max_cards = max_cards;
+        this._limit_legend = limit_legend;
+        this._limit_epic = limit_epic;
+        this._limit_rare = limit_rare;
+        this._limit_item = limit_item;
+        this.issues = [];
     }
 
     public validate(cards: ICard[]): IDeckValidity {
-        throw new Error("Method not implemented.");
+        this.issues = [];
+        this.checkTotalCards(cards);
+        this.checkItemCount(cards);
+        this.checkRarity(cards);
+        return new DeckValidity(this.issues.length === 0, this.issues);
+    }
+
+    /**
+     * check that the amount of cards of rarities are within the rules
+     * @param cards deck to validate
+     */
+    private checkItemCount(cards: ICard[]): void {
+        let item: number = 0;
+        for (let card of cards) {
+            if (card instanceof CardItem) {
+                item++;
+            }
+        }
+        if (item > this._limit_item) {
+            this.issues.push(DeckIssue.STR_LIMIT_ITEM);
+        }
+    }
+
+    /**
+     * check that the amount of cards of rarities are within the rules
+     * @param cards deck to validate
+     */
+    private checkRarity(cards: ICard[]): void {
+        let counts: number[] = new Array(Object.keys(CardRarity).length).fill(0);
+        for (let card of cards) {
+            counts[card.rarity]++;
+        }
+        if (counts[CardRarity.LEGENDARY] > this._limit_legend) {
+            this.issues.push(DeckIssue.STR_LIMIT_LEGEND);
+        }
+        if (counts[CardRarity.EPIC] > this._limit_epic) {
+            this.issues.push(DeckIssue.STR_LIMIT_EPIC);
+        }
+        if (counts[CardRarity.RARE] > this._limit_rare) {
+            this.issues.push(DeckIssue.STR_LIMIT_RARE);
+        }
+    }
+
+    /**
+     * check that the total number of cards is within the rules
+     * @param cards deck to validate
+     */
+    private checkTotalCards(cards: ICard[]): void {
+        if (cards.length < this._min_cards) {
+            this.issues.push(DeckIssue.STR_MIN_CARDS);
+        }
+        if (cards.length > this._max_cards) {
+            this.issues.push(DeckIssue.STR_MAX_CARDS);
+        }
     }
     
 }
