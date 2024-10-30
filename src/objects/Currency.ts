5bc42ae196e2e27c1997b8f1cd78568c4e0a1924
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-30 02:11:07 -0500

    completed currency object

--- src/objects/Currency.ts
+++ src/objects/Currency.ts
@@ -1,11 +1,12 @@
+import { Wallet } from "../rest/payloads";
 import { ICurrency, IItem, IItemVisitor } from "./index";
 
 export class Currency implements ICurrency, IItem {
     private readonly _coins: number;
     
-    //TODO: need to have a from payload method
-    // https://github.com/InternetEnemies/combatcritters-ts/issues/66
-    // We do not have currency task on the list, so this will be the best option
+    public static fromWalletPayload(payload: Wallet): Currency {
+        return new Currency(payload.coins);
+    }
 
     constructor(coins: number) {
         this._coins = coins;
