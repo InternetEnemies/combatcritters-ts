50a1be290487fd965151e60a0058aaf3a317bced
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-11-03 17:30:45 -0600

    created currency integration test

---
+++ test/integration/Currency.spec.ts
@@ -0,0 +1,26 @@
+import {BackendInstance} from "./ContainerUtil";
+import {IClient, IUserPack} from "../../src";
+import * as assert from "node:assert";
+
+let client: IClient;
+let container: BackendInstance
+
+beforeEach(async () => {
+    container = new BackendInstance()
+    client = await container.init();
+    await client.register("username","password")
+    await client.login("username","password")
+})
+describe("Currency test", () => {
+
+    it("get user wallet", async() => {
+        let coinsObj = await client.user.currency.getCurrency();
+        let coins = coinsObj.coins;
+        assert.ok(coins === 0);
+    })
+
+})
+
+afterEach(async () => {
+    await container.teardown();
+})
