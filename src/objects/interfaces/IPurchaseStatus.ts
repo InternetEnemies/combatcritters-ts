4701f68d4a087723370283bbe776aa30157e4510
jackal <v@njackal.com>
2024-11-05 12:45:00 -0600

    purchase now returns full vendor rep

--- src/objects/interfaces/IPurchaseStatus.ts
+++ src/objects/interfaces/IPurchaseStatus.ts
@@ -1,9 +1,10 @@
+import {IVendorReputation} from "./IVendorReputation";
+
 /**
  * Interface for the PurchaseStatus object
  * The status of Offer.accept()
  */
 export interface IPurchaseStatus {
     isCompleted: boolean;
-    vendorID: number;
-    xpChange: number;
+    reputation: IVendorReputation
 }
