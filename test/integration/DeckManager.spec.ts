5b8601a6c3029d09aeaef5f60a49b63b008d1b05
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-11-11 03:27:41 -0600

    updated Deck Manager integration test

--- test/integration/DeckManager.spec.ts
+++ test/integration/DeckManager.spec.ts
@@ -1,20 +1,46 @@
 import {BackendInstance} from "./ContainerUtil";
-import {IClient, IUserPack} from "../../src";
+import {IClient, IDeck, IDeckManager, IUserPack} from "../../src";
 import * as assert from "node:assert";
 
 let client: IClient;
-let container: BackendInstance
+let container: BackendInstance;
+let manager: IDeckManager;
 
 beforeEach(async () => {
     container = new BackendInstance()
     client = await container.init();
     await client.register("username","password")
     await client.login("username","password")
+    manager = client.user.decks;
 })
 describe("Deck Manager test", () => {
 
-    it("User can open a pack", async () => {
+    it("User has zero deck at init", async () => {
+        let decks:IDeck[] = await manager.getDecks();
+        assert.ok(decks.length === 0)
+    })
 
+    it("User can create a deck", async () => {
+        let deck:IDeck = await manager.createDeck("deckname");
+        assert.ok(deck.name === "deckname")
+        let decks:IDeck[] = await manager.getDecks();
+        assert.ok(decks.length === 1)
+    })
+
+    it("User can have multiple decks", async () => {
+        let deck:IDeck = await manager.createDeck("deckname");
+        let deck2:IDeck = await manager.createDeck("deckname2");
+        let decks:IDeck[] = await manager.getDecks();
+        assert.ok(decks.length === 2)
+    })
+
+    it("User can delete a deck", async () => {
+        let deck:IDeck = await manager.createDeck("deckname");
+        let decks:IDeck[] = await manager.getDecks();
+        assert.ok(decks.length === 1)
+        await manager.deleteDeck(deck);
+        decks = await manager.getDecks();
+        assert.ok(decks.length === 0)
     })
 })
 
