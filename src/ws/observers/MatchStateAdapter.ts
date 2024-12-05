1f5fd995981fda4d685b30ff573f17e24a0d88ac
VanJackal <v@njackal.com>
2024-12-05 17:08:45 -0600

    rewards handling

--- src/ws/observers/MatchStateAdapter.ts
+++ src/ws/observers/MatchStateAdapter.ts
@@ -1,14 +1,19 @@
 import {IMatchStateObserver} from "./IMatchStateObserver";
 import {HandlerSet} from "../ICritterSocket";
 import {GameFoundEvent, MatchEndedEvent} from "../payloads/MatchPayloads";
+import {IRest} from "../../rest";
+import {ItemStack, Offer} from "../../objects";
 
-export function getMatchStateAdapter(matchStateObserver:IMatchStateObserver): HandlerSet {
+export function getMatchStateAdapter(matchStateObserver:IMatchStateObserver, rest:IRest): HandlerSet {
     return {
         game_found_event:(body:GameFoundEvent) => {
             matchStateObserver.gameFound(body.opponent)
         },
         match_ended_event:(body:MatchEndedEvent) => {
-            matchStateObserver.matchEnded(body.type)
+            let items = body.rewards.map((reward) => {
+                return new ItemStack(Offer.fromOfferItemPayload(reward,rest),reward.count)
+            })
+            matchStateObserver.matchEnded(body.type, items)
         }
     }
 }
